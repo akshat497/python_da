7d371a7b30df19101c1554902671e57a0e3ff17d
Akshat saini <akshatsaini@Akshats-MacBook-Air.local>
2026-01-12 12:29:29 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,107 @@
+# a=[1,2,3]#memory address
+# b=a
+# c=[1,2,3]
+# print(a is b)  # True, because b is a reference to the same list as a
+# print(a is c)  # False, because c is a different list with the same contents
+# print(a == c)  # True, because the contents of a and c are the same
+# print(b == c)  # True, because the contents of b and c are the same
+# print(a is not c)  # True, because a and c are different objects
+
+# a=100
+# b=22
+# a/= b
+# print(a) 
+
+# marks=54
+
+# print("Pass") if marks >= 50 else print("Fail")
+
+# #check number is positive of negative
+# num=5
+
+# if(num>0):
+#     print("Positive")
+# elif(num<0):
+#     print("Negative")
+# else:
+#     print("Zero")
+    
+    #check even or odd
+# num=4   
+# if(num%2==0):
+#     print("Even")
+# else:
+    # print("Odd")
+    
+# login=True
+# member="customer"
+    
+# if login:
+#         if member=="admin":
+#             print("Welcome",member)
+#         else:
+#             print("Welcome ",member)
+# else:
+#         print("Please login") 
+        
+        
+# Check whether a number is positive or negative.
+
+# Check whether a number is even or odd.
+
+# Find the greater of two numbers.
+
+# Check whether a person is eligible to vote (age ≥ 18).
+
+# Check whether a year is a leap year.
+
+
+
+
+
+
+
+
+# Check whether a number is a multiple of 10.
+
+# Check whether a character is uppercase or lowercase.
+
+# Check whether a number is a two-digit number.
+
+# Check whether a number is divisible by 7.
+
+# Check whether a person is eligible for a driving license (age ≥ 18).
+
+# Check whether a number is positive, negative, or zero.
+
+# Check whether a character is a digit or alphabet.
+
+# Check whether a given temperature is hot (>30°C), normal (15–30°C), or cold (<15°C).
+
+# Check whether a number lies between 1 and 100 (inclusive).
+# first=int(input("enter a number"))
+year=int(input("enter a number"))
+
+if year%4==0:
+    print("leap year")  
+else:
+    print("not a leap year")
+# if first>second:
+#     print("first is greater")
+# elif    first<second:
+#     print("second is greater")
+# else:
+#     print("both are equal")
+
+
+# if number%2==0:
+#     print("number is even")
+# else:
+#     print("number is odd")   
+
+# if(number>0):
+#     print("Positive")   
+# else:
+#     print("negative")
+    
+    
