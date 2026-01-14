0eb43559dd4704c18070f46ccf956b435a787aee
Akshat Saini <akshatsaini497@gmail.com>
2026-01-14 12:24:08 +0530

    loops in python

---
+++ loops.py
@@ -0,0 +1,27 @@
+toy_box=["car","bike",25,45.67] #list
+        #  0     1     2   3  
+#for loop
+# print(toy_box[2])
+# print(toy_box)
+# for key in toy_box:
+#     print(key)
+
+
+# for key in range(0,40,4):
+#     print(key)
+
+
+# name="akshat"
+
+# for char in range(0,10):
+#     print(name[char])
+
+
+
+information={"name":"akshat","age":24,"phone":7056275526}#dictionary   you can call it a key value pair also
+            #key     value
+
+
+print(information["name"])
+for i in information:
+    print(i,":",information[i])
