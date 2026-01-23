e27278b10335fc465314503a123fbe0743b11356
Akshat saini <akshatsaini@Akshats-MacBook-Air.local>
2026-01-23 12:06:16 +0530

    new features added in library management program

--- library_management.py
+++ library_management.py
@@ -1,3 +1,6 @@
+import datetime
+from datetime import date
+
 # lets make a library management program with functions and loops
 books=[
     {"id":1, "title":"1984", "author":"George Orwell", "available":True,"issued_to":None},
@@ -26,28 +29,34 @@ def viewStudents():
 def issueBook():
        book_id = int(input("Enter book ID to issue: "))
        students_id= int(input("Enter student ID: "))
+       days= int(input("Enter number of days: "))
+       print("you can issue book for maximum 10 days")
+       if days >10 and days<1:
+           print("Cannot issue book for more than 10 days or less than 1 day.")
+           return False
        for book in books:
            if book_id==book['id']:
                if book['available']:
                    
                    for student in students:
                           if students_id==student["id"]:
-                            student['borrowed_books'].append(book["id"])
                             book['issued_to']=student['id']
                             book['available']=False
+                            student['borrowed_books'].append({"borrowed_date":date.today(),"return_date":None, "how_many_days_left":days,"id":book['id']})
+
                             print(f"Book '{book['title']}' issued to {student['id']}")
                             return True
-                          else:
-                            print("Student ID not found.")
-                            return False
-               else:
-                  print("Book is not available for issuing.")
-                  return False
-           else:
-                print("Book ID not found.")      
-                return False
+        #                   else:
+        #                      print("Student ID not found.")
+        #                      return False
+        #            else:
+        #              print("Book is not available for issuing.")
+        #              return False
+        #    else:
+        #         print("Book ID not found.")      
+        #         return False
                             
-                        
+                      
 def returnBook():
     book_id=int(input("Enter book ID to return: "))
     for book in books:
@@ -56,21 +65,52 @@ def returnBook():
                 student_id=book['issued_to']
                 for student in students:
                     if student_id==student['id']:
-                        for id in student['borrowed_books']:
-                            if id==book['id']:
-                                student['borrowed_books'].remove(book['id'])
-                                book['issued_to']=None
+                        for bookDetails in student['borrowed_books']:
+                            if bookDetails['id']==book_id:
+                                bookDetails['return_date']=date.today()
+                                days=(bookDetails['return_date'] - bookDetails['borrowed_date']).days
+                                bookDetails['how_many_days_left']-=days
+                                if bookDetails['how_many_days_left']<0:
+                                    fine=abs(bookDetails['how_many_days_left'])*10
+                                    print(f"Book is returned late. Fine to be paid: {fine} units.")
+                                else:
+                                    print("Book returned on time. No fine.")
+                                student['borrowed_books'].remove(bookDetails)
                                 book['available']=True
+                                book['issued_to']=None
+                           
+                                
                                 print(f"Book '{book['title']}' returned by {student['id']}")
                                 return
                     
+def addNewBook():
+    title=input("Enter book title: ")
+    author=input("Enter book author: ")
+    new_id=len(books)+1
+    books.append({"id":new_id,"title":title,"author":author,"available":True,"issued_to":None})
+    print(f"Book '{title}' added successfully with ID {new_id}.") 
     
+def removeBook():   
+    book_id=int(input("enter bookid to remove: "))
+    reason=input("enter reason to remove book: ")
+    for book in books:
+        if book['id']==book_id:
+            if book['available']:
+                books.remove(book)
+                print(f"Book '{book['title']}' removed successfully for reason: {reason}.")
+                return
+            else:
+                print("Cannot remove book. It is currently issued to a student.")
+                return
+    print("Book ID not found.")
 while True: 
     print("1. View Books")
     print("2. View Students")
     print("3. Issue Book")
     print("4. Return Book")
-    print("5. Exit")
+    print("5 to add new book")
+    print("6 to remove  book")
+    print("7. Exit")
     
     choice=int(input("Enter your choice: "))
     
@@ -85,5 +125,10 @@ while True:
     elif choice==4:
         returnBook() 
         viewStudents()   
-    if choice==5:
+    elif choice==5:
+        addNewBook()
+
+    elif choice==6:
+        removeBook()
+    elif choice==7:
         break
